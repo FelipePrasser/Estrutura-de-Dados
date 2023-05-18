c76eb967678ec00257b05bdeb1c97d3b6b8de18f
FelipePrasser <92392630+FelipePrasser@users.noreply.github.com>
2023-05-17 23:15:30 -0300

    Update TesteArvoreBinaria.java

--- Atividade6/TesteArvoreBinaria.java
+++ Atividade6/TesteArvoreBinaria.java
@@ -4,5 +4,11 @@ public class TesteArvoreBinaria {
     public static void main(String[] args) {
         ArvoreBinaria arvore=new ArvoreBinaria();
         arvore.insere(1, arvore.insere(2, null, arvore.insere(4, null, null)), arvore.insere(3, arvore.insere(5, null, null), arvore.insere(6, null, null)));
+        System.out.println("Pré Ordem:\n"+arvore.imprimePre());
+        System.out.println("Ordem Simétrica:\n"+arvore.imprimeSim(arvore.getRaiz()));
+        System.out.println("Pós Ordem:\n"+arvore.imprimePos(arvore.getRaiz()));
+        System.out.println("Numeros Nos:\n"+arvore.numNos(arvore.getRaiz()));
+        System.out.println("Altura:\n"+arvore.altura(arvore.getRaiz()));
+        System.out.println("Folhas:\n"+arvore.folhas(arvore.getRaiz()));
     }
 }
